532620957294ab48d39ab6a27597bad31a6d14a1
JR Strayhorn <jrstrayhorn@gmail.com>
2018-06-26 16:32:42 -0400

    done

--- EFReview/Program.cs
+++ EFReview/Program.cs
@@ -2,6 +2,7 @@
 
 namespace EFReview
 {
+
     class Program
     {
         static void Main(string[] args)
@@ -11,8 +12,10 @@ namespace EFReview
             //dbContext.AddVideo("Black Panther", new DateTime(2018, 2, 16), "Action");
             //dbContext.AddVideo("Wedding Crashers", new DateTime(2000, 3, 5), "Comedy");
 
-            dbContext.AddVideo("Iron Man", new DateTime(2008, 4, 1), "Action");
-            dbContext.AddVideo("Thor Ragnarok", new DateTime(2017, 11, 10), "Comedy");
+            //dbContext.AddVideo("Iron Man", new DateTime(2008, 4, 1), "Action");
+            //dbContext.AddVideo("Thor Ragnarok", new DateTime(2017, 11, 10), "Comedy");
+
+            dbContext.AddVideo("Guardians of Galaxy Vol 2", new DateTime(2017, 3, 5), "Comedy", (byte)Classification.Gold);
         }
     }
 }
--- EFReview/Video.cs
+++ EFReview/Video.cs
@@ -18,6 +18,7 @@ namespace EFReview
         public string Name { get; set; }
         public System.DateTime ReleaseDate { get; set; }
         public byte GenreId { get; set; }
+        public EFReview.Classification Classification { get; set; }
     
         public virtual Genre Genre { get; set; }
     }
