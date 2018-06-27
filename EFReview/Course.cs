d47261f7acc121568bb7db590b811744ca91f07e
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-27 07:49:22 -0400

    wip

--- EFReview/Course.cs
+++ EFReview/Course.cs
@@ -17,7 +17,7 @@ namespace EFReview
 
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
