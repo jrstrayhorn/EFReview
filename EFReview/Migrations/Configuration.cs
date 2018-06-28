ffd2777fe6ed3264ad0a332b22988a71a050f76d
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-28 06:44:40 -0400

    done

--- EFReview/Migrations/Configuration.cs
+++ EFReview/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace EFReview.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -12,6 +13,14 @@ namespace EFReview.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        // we use this to supply dummy data to our development database
+        // should not store reference data here
+        // if you have reference data that you need to populate
+        // you should create an empty migration and then use the SQL()
+        // command to populate the database
+
+        // this seed method is ran everytime after update-database executes
+        // any pending migrations
         protected override void Seed(EFReview.PlutoContext context)
         {
             //  This method will be called after migrating to the latest version.
@@ -26,6 +35,16 @@ namespace EFReview.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course() { Name = "Course for Author 1", Description = "Description" }
+                    }
+                });
         }
     }
 }
