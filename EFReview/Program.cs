a70b530c961ad04332d263b80f88ba64e33349af
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-28 07:06:25 -0400

    done with interation 1

--- EFReview/Program.cs
+++ EFReview/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFReview
 {
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public IList<Genre> Genres { get; set; }
+    }
+
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public IList<Video> Videos { get; set; }
+    }
+
+    public class VidzyContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
