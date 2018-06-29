f9f040dfd15f3f2f5bed4cd66a9b3890297beddd
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-29 07:48:18 -0400

    done

---
+++ EFReview/Course.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFReview
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+    }
+}
---
+++ EFReview/PlutoContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace EFReview
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext()
+            : base("name=PlutoFresh")
+        {
+
+        }
+    }
+}
