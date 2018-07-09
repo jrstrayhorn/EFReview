329de97610cb61006658356ead2a6a1ff8a02653
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-09 07:47:37 -0400

    wip

--- EFReview/PlutoContext.cs
+++ EFReview/PlutoContext.cs
@@ -35,7 +35,12 @@ namespace EFReview
             modelBuilder.Entity<Course>()
                     .HasMany(c => c.Tags)
                     .WithMany(t => t.Courses)
-                    .Map(m => m.ToTable("CourseTags"));
+                    .Map(m => 
+                    {
+                        m.ToTable("CourseTags");
+                        m.MapLeftKey("CourseId");
+                        m.MapRightKey("TagId");
+                    });
 
             modelBuilder.Entity<Course>()
                     .HasRequired(c => c.Cover)
