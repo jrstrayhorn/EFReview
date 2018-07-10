b75d16217b5b8a9ac4097a4e61c519e5af18a052
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-10 07:11:49 -0400

    wip - start

---
+++ EFReview/PlutoContext.cs
@@ -0,0 +1,27 @@
+﻿using EFReview.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFReview
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoLINQContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
