21fc065f28084e732df9803d5377cd1007d88c31
stepforwardtech <jrstrayhorn@gmail.com>
2018-07-11 07:42:32 -0400

    wip

--- EFReview/Program.cs
+++ EFReview/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,18 @@ namespace EFReview
     {
         static void Main(string[] args)
         {
+            var context = new VidzyContext();
+
+            Console.WriteLine("***Action movies sorted by name");
+
+            var actionMovies = context.Videos
+                .Where(v => v.Genres.Name == "Action")
+                .OrderBy(v => v.Name);
+
+            foreach (var v in actionMovies)
+            {
+                Console.WriteLine(v.Name);
+            }
         }
     }
 }
